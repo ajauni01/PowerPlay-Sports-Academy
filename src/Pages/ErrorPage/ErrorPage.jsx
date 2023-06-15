e5a7fc790ba8b1935b219bbf230c597bb829ab81
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-06-15 10:33:00 -0500

    initial-setup-completed

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <p>Opps! 404</p>
+
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,21 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h2> Welcome to the Home</h2>
+
+
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+
+    </div>
+  );
+};
+
+export default Home;
