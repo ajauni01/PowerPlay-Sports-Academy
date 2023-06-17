a264531706ecdd81ec15348395e3a269ff8a46e0
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-06-16 23:06:46 -0500

    dashboard-condition-added

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -7,7 +7,6 @@ const Dashboard = () => {
   const { data } = useUserAuthorization()
   console.log('user role from the backend', data)
 
-
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -23,7 +22,9 @@ const Dashboard = () => {
         <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
 
           {/* put conditions here depending on the nature of user */}
-          <p>admin</p>
+          {data?.role === 'admin' && <p>Admin Dashboard</p>}
+          {data?.role === 'instructor' && <p>Instructor Dashboard</p>}
+          {data?.role === 'student' && <p>Student Dashboard</p>}
 
         </ul>
       </div>
