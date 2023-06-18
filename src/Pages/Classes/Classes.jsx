da0173567cc8669c044b03210585a6110f0f13b7
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-06-18 02:41:58 -0500

    all-classes-added

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,14 +1,33 @@
-
+import { useEffect, useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import PopularClassCard from "../Home/PopularClass/PopularClassCard";
 
 
 const Classes = () => {
 
+  const [allClasses, setAllClasses] = useState([])
+
+  useEffect(() => {
+    fetch('https://sports2-orcin.vercel.app/popularClasses')
+      .then(res => res.json())
+      .then(data => setAllClasses(data));
+  }, []);
+
 
   return (
-    <div className="mt-10">
+    <div className="w-[80%] mx-auto mt-24">
+      <div className="animate__fadeInDown text-7xl mb-14 text-center">
+        <Fade cascade delay={200}>
+          <span className='text-red-600 font-bold'>Our Top Classes</span>
+        </Fade>
+      </div>
 
-      <p>Classes Page</p>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mb-24 ms-20 '>
+        {
+          allClasses.map(cls => <PopularClassCard key={cls._id} cls={cls}></PopularClassCard>)
+        }
 
+      </div>
 
     </div>
   );
